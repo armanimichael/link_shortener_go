e1be440178e80183e16d65f876d7a627689195ad
Michael Armani <michaelarmani00@gmail.com>
2021-10-23 19:50:41 +0200

    Add basic route

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "Index")
+	})
+	router.Run()
 }
