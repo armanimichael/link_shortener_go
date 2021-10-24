7f6fa6b1dae97405988de8fa20c2f481a6b6e5f8
Michael Armani <michaelarmani00@gmail.com>
2021-10-24 20:51:43 +0200

    Test shortener package

---
+++ shortener/shortener_test.go
@@ -0,0 +1,52 @@
+package shortener
+
+import "testing"
+
+const length int = 10
+const allowedCharsCount int = 62
+
+func TestAllowedChars(t *testing.T) {
+	allowed := getAllowedChars()
+
+	if len(allowed) != allowedCharsCount {
+		t.Errorf("Allowed characters should be %d", allowedCharsCount)
+	}
+}
+
+func TestRandomCombination(t *testing.T) {
+	combination := GetRandomCombination(length)
+
+	combinationToStr := ""
+	for _, item := range combination {
+		combinationToStr += string(item)
+	}
+	t.Logf("\n%s", combinationToStr)
+}
+
+func TestRandomicity(t *testing.T) {
+	var combinations [1000000]string
+
+	for i := 0; i < len(combinations); i++ {
+		random := GetRandomCombination(10)
+		combinations[i] = string(random)
+	}
+
+	collisionCount := 0
+	collisions := map[string]int{
+		combinations[0]: 0,
+	}
+
+	for i, item := range combinations {
+		if i == 0 {
+			continue
+		}
+
+		if _, exists := collisions[item]; !exists {
+			collisions[item] = i
+		} else {
+			collisionCount++
+		}
+	}
+
+	t.Logf("\nCollisions: %d", collisionCount)
+}
