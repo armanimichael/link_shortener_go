ea15f6cc31eda8573519cdc8887aea02758e170f
Michael Armani <michaelarmani00@gmail.com>
2021-10-30 13:46:36 +0200

    Test migrate db

--- database/db_connection_test.go
+++ database/db_connection_test.go
@@ -2,15 +2,33 @@ package database
 
 import (
 	"fmt"
+	"github.com/armanimichael/link_shortener_go/database/models"
 	"testing"
 )
 
-func TestNewDatabase(t *testing.T) {
+func getDB() *database {
 	conn := fmt.Sprintf("../%s", defaultConnection)
 	db := NewDatabase(conn)
-	err := db.connect()
+	return db
+}
+
+func TestNewDatabase_Connect(t *testing.T) {
+	db := getDB()
+	err := db.Connect()
 
 	if err != nil {
 		t.Errorf("failed to connect database")
+	}
+}
+
+func TestDatabase_Migrate(t *testing.T) {
+	db := getDB()
+	db.Connect()
+	db.Migrate()
+
+	var links []models.Link
+	result := db.connection.Find(&links)
+	if result.Error != nil {
+		t.Errorf("table link does not exist")
 	}
 }
