d5722794f77d851f226739bd127ac6463a9e2b26
Michael Armani <michaelarmani00@gmail.com>
2021-10-31 00:26:00 +0200

    Validate url on short link creation

--- routes/api.go
+++ routes/api.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/armanimichael/link_shortener_go/database/dals"
+	httpUtils "github.com/armanimichael/link_shortener_go/http_utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -13,13 +14,26 @@ func apiRoutes(g *gin.RouterGroup) {
 func generateLink(context *gin.Context) {
 	url := context.PostForm("link")
 
-	short, alreadyExists := dals.GetShortLink(url)
-	if !alreadyExists {
-		short = dals.SetShortLink(url)
+	if !httpUtils.IsValidUrl(url) {
+		invalidLinkResponse(context)
+	} else {
+		short, alreadyExists := dals.GetShortLink(url)
+		if !alreadyExists {
+			short = dals.SetShortLink(url)
+		}
+		validLinkResponse(short, url, context)
 	}
+}
 
+func validLinkResponse(short string, originalLink string, context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{
 		"short":    short,
-		"original": url,
+		"original": originalLink,
+	})
+}
+
+func invalidLinkResponse(context *gin.Context) {
+	context.JSON(http.StatusUnprocessableEntity, gin.H{
+		"message": "invalid url",
 	})
 }
